b20987f86a660dfaedc21354019fab9c227063ff
Weiss Bai <whitesgr03@gmail.com>
2023-05-13 17:45:57 +0800

    Add initial value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-	constructor() {}
+	constructor() {
+		super();
+		this.state = {
+			personal: {
+				type: "personal",
+				data: {
+					firstName: "Your",
+					lastName: "Name",
+					email: "example@gmail.com",
+					phone: "(555) 555-5555",
+					city: "City",
+					state: "ST",
+				},
+			},
+			employment: {
+				type: "employment",
+				data: {
+					id: 1,
+					jobTitle: "Job Title",
+					employer: "Company Name",
+					startDate: "Month Year",
+					endDate: "Month Year",
+					city: "City",
+					state: "ST",
+					describes: [{ id: 1, text: "Core responsibility" }],
+				},
+				dataList: [
+					{
+						id: 1,
+						jobTitle: "Job Title",
+						employer: "Company Name",
+						startDate: "Month Year",
+						endDate: "Month Year",
+						city: "City",
+						state: "ST",
+						describes: [{ id: 1, text: "Core responsibility" }],
+					},
+				],
+			},
+			education: {
+				type: "education",
+				data: {
+					id: 1,
+					school: "University Name",
+					degreeMajors: "Degree, Majors",
+					graduationDate: "Month, Year",
+					city: "City",
+					state: "ST",
+					describes: [{ id: 1, text: "Honors or fun stuff" }],
+				},
+				dataList: [
+					{
+						id: 1,
+						school: "University Name",
+						degreeMajors: "Degree, Majors",
+						graduationDate: "Month, Year",
+						city: "City",
+						state: "ST",
+						describes: [{ id: 1, text: "Honors or fun stuff" }],
+					},
+				],
+			},
+			skills: {
+				type: "skills",
+				data: {
+					text: "Important skills or abilities required to fulfill the task role.",
+				},
+			},
+		};
+	}
 	render() {
 		return <div></div>;
 	}
